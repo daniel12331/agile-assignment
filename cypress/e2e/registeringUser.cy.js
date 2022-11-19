97c4fd2541a347b6574a06975def02b10c1c4d3a
Daniel <20088708@wit.ie>
2022-11-19 22:49:08 +0000

    Created Login Testing and edited Register User testing.

--- cypress/e2e/registeringUser.cy.js
+++ cypress/e2e/registeringUser.cy.js
@@ -1,5 +1,3 @@
-let movies; // List of movies from TMDB
-let movie; //
 
 describe("Register User", () => {
 
@@ -9,32 +7,39 @@ describe("Register User", () => {
   const cpassword = "123456"
 
   describe("Register an account", () => {
+    
     it("Check if user directed to landing page when visiting the home page", () => {
      cy.visit("/")
      cy.get("a").contains("Not a member yet?").click();
     
     });
+
     it("Create an account", () => {
      
-
       cy.get("#name").clear().type(name);   
       cy.get("#email").clear().type(email); 
       cy.get("#password").clear().type(password);
       cy.get("#confirmpasssword").clear().type(cpassword); 
 
       cy.get("button").contains("Register").click();  
+      cy.url().should("eq", `http://localhost:3000/`);
      
      });
-     it("Check if we are directed to home page and check if username is correct", () => {
-      cy.url().should("eq", `http://localhost:3000/`);
-      cy.get(".MuiTypography-h4").contains(email)
+    });
+
+    describe("Check if registration was successful", () => {
       
+     it("Check if username is correct", () => {
+      cy.get(".MuiTypography-h4").contains(email)
+    
      });
-     it("The logout and check if we are back at landing page", () => {
+
+     it("Then logout and check if we are back at landing page", () => {
       cy.get("button").contains("Logout").click()
 
       cy.url().should("eq", `http://localhost:3000/landing`);
      });
-  });
+
+   });
 
 });
